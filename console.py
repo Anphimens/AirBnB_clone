ef74b8b49bf2dc8a3649b931377b12108d1d235c
Anphimens <philipinamensah19@gmail.com>
2024-02-04 19:45:10 +0000

    Command interpreter

--- console.py
+++ console.py
@@ -1,25 +1,54 @@
 #!/usr/bin/env python3
 # console.py
 import cmd
+from models.base_model import BaseModel
 
 
-class OurCmd(cmd.Cmd):
+class dHBNBComman(cmd.Cmd):
     """This is simple command interpreter of Antoinette and Eugenious"""
-    intro = "You are welcome to OurCmd, explore for more features"
-    prompt = "(OurCmd) "
+    prompt = "(hbnb) "
 
-    def do_greet(self, person):
-        """Greets the user"""
-        if person:
-            print(f"hi {person} you are welcome, how may we help you?")
-        else:
-            print("Hi, you are welcome. How may we help you?")
+
+    __models = {
         
+        "BaseModel": BaseModel 
+
+    }
+
+
     def emptyline(self):
+        """Prints a new line when the user enters the enter key"""
         pass
 
-    def do_EOF(self, line):
+    def do_quit(self, line):
+        """Quit command to exit the program"""
         return True
 
+    def do_EOF(self, line):
+        """This function exits the cmd"""
+        return True
+    
+    def do_create(self, line):
+        """This method create a new instance of the BaseModel:
+        Saves it to the JSON file and the prints the id.
+
+        It prints message when no name found or name does not exist
+        """
+        if not line:
+            print("** class name missing **")
+            return
+        class_name = line.split()[0]
+        if class_name not in self.__models:
+            print("** class doesn't exist **")
+            return
+        new_instance = BaseModel()
+        new_instance.save()
+        print(new_instance.id)
+
+
+
+
+
+
 if __name__ == '__main__':
-    OurCmd().cmdloop()
+    dHBNBComman().cmdloop()
